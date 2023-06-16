87bfd7a4490a8559c4abc6294b394ea54ad42928
XF <118357460+wqfa@users.noreply.github.com>
2023-06-16 09:23:37 +0300

    Create stringiqthon.py

---
+++ stringiqthon.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# (c) https://t.me/c45cs
+
+# This Source Code Form is subject to the terms of the GNU
+
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+
+from telethon.sessions import StringSession
+
+from telethon.sync import TelegramClient
+
+print("- Code telethon For ch : @c45cs")
+
+APP_ID = int(input("APP ID here: "))
+
+API_HASH = input("API HASH here: ")
+
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+
+    print(client.session.save())
+
+    client.send_message("me", client.session.save())
